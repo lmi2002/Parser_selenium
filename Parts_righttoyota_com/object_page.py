6f0f8c3db28bd94ae9c5dd954f70aaf1501c2922
Vitaliy Lanin <vitaliy.lanin@exist.ua>
2019-01-28 14:05:49 +0200

    2019-01-28

--- Parts_righttoyota_com/object_page.py
+++ Parts_righttoyota_com/object_page.py
@@ -8,6 +8,7 @@ import os
 import csv
 import logging
 from functools import reduce
+import urllib.request
 
 from helper import logger
 from properties.prop_driver import Driver
@@ -33,14 +34,46 @@ class PageObjectPartsRightToyota(Driver):
     def get_secondary_images(self):
         return self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'secondary-images')))
 
+    def get_main_image(self):
+        return self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.main-image > a img')))
+
+    def get_product_details_inner(self):
+        return self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.product-details-inner')))
+
 
 if __name__ == "__main__":
 
+    # add path to save images
+    path_to_save_img = 'C:\\Users\\anokhin\\Desktop\\'
+    art = '43512-06150'
     pop = PageObjectPartsRightToyota()
     pop.open_site(pop.url)
     pop.maximize_window()
-    pop.get_input().send_keys('43512-06150')
+    pop.get_input().send_keys(art)
     pop.get_input().send_keys(Keys.ENTER)
-    print(pop.get_secondary_images())
+
+    secondary_images = pop.get_secondary_images()
+
+    images_list = [img.get_attribute('src') for img in secondary_images.find_elements_by_css_selector('li > a img')]
+
+    # save to folder img
+    for ind, href_img in enumerate(images_list, start=1):
+        urllib.request.urlretrieve(href_img, path_to_save_img + art + '_' + str(ind) +
+                                   href_img[href_img.rfind("."):])
+
+    # save to folder main_img
+    main_image = pop.get_main_image().get_attribute('src')
+    urllib.request.urlretrieve(main_image, path_to_save_img + art + '_' + str(0) +
+                               href_img[href_img.rfind("."):])
+
+    product_details = pop.get_product_details_inner()
+
+    for detail in product_details.find_elements_by_css_selector('li'):
+            print(detail.find_element_by_css_selector('label').text)
+            print(detail.find_element_by_css_selector('span').text)
 
 
+
+
+
+pop.close()
